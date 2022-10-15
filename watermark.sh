d99b68b3c60b96d332701de6d22e02257ddda537
Oscar Koeroo <okoeroo@gmail.com>
2022-10-15 21:37:36 +0200

    Text seperated

--- watermark.sh
+++ watermark.sh
@@ -2,6 +2,10 @@
 
 export LC_CTYPE="en_US.UTF-8"
 
+TEXT_CENTER="© Sandra van de Luitgaarden"
+TEXT_LOW_LEFT="Celebrities Spotted"
+
+
 
 if [ -z "$1" ]; then
     echo "Error: no filename provided"
@@ -24,11 +28,11 @@ echo "Input: ${original_image} - processing"
 
 convert "${original_image}" \
    -fill 'rgba(100%,100%,100%,0.2)' -pointsize 400 \
-   -gravity Center -draw "text 0,50 '© Sandra van de Luitgaarden'" "${new_image}.step_1.TMP"
+   -gravity Center -draw "text 0,50 '${TEXT_CENTER}'" "${new_image}.step_1.TMP"
 
 convert "${new_image}.step_1.TMP" \
    -fill 'rgba(100%,100%,100%,0.4)' -pointsize 200 \
-   -gravity SouthWest -draw "text 0,50 'Celebrities Spotted'" "${new_image}.step_2.TMP"
+   -gravity SouthWest -draw "text 0,50 '${TEXT_LOW_LEFT}'" "${new_image}.step_2.TMP"
 
 
 # Final file
